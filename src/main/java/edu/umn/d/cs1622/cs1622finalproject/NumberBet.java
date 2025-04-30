1329aac6003fbe7240b23723eb5563ad38bf6c58
Max <monze010@umn.edu>
2025-04-29 19:58:57 -0500

    Number bet of roulette bets complete

---
+++ src/main/java/edu/umn/d/cs1622/cs1622finalproject/NumberBet.java
@@ -0,0 +1,21 @@
+package edu.umn.d.cs1622.cs1622finalproject;
+
+public class NumberBet extends RouletteBet{
+    private int number;
+
+    public NumberBet(int amt, int number) {
+        super(amt);
+        this.number = number;
+        this.description = "Single Number: " + number;
+    }
+
+    @Override
+    public boolean isWinner(int result) {
+        return result == number;
+    }
+
+    @Override
+    public int getPayoutMultiplier() {
+        return 35;
+    }
+}
